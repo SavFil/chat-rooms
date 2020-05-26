2c92b39dfd9f793e4ca6077157567eb96871a52c
Σαββας Φιλιππιδης <filippides.savvas@gmail.com>
2020-05-26 13:26:31 +0300

    Added project Chat-Rooms
    Minimalist multiroom chat server, made in node.js. Reference by Node.js in action book

---
+++ chat-rooms/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+const cache = {};
+
+function send404(response) {
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.write('Error 404: resource not found.');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(200, { 'Content-Type': mime.lookup(path.basename(filePath)) });
+    response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exist(absPath, (exists) => {
+            if (exists) {
+                fs.readFile(absPath, (err, data)=>{
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            }
+        });
+    }
+    
+
+}
